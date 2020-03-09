9eab188b7d83ddd799a5c0e0d094a13993c50f42
Ngocvu1808 <vunguyen188hk@gmail.com>
2020-03-09 10:23:11 +0700

    Add app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+// console.log("hello world!");
+
+// setTimeout(function(){
+//     console.log("After 3 second take!");
+// }, 10000);
+
+var time = 0;
+setInterval(function(){
+    time+=2;
+    console.log(time +"test!");
+}, 3000);
